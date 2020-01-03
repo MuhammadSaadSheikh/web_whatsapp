eccd63f91488d8322f3b29388ba6141c5de777b9
MuhammadSaadSheikh <46067347+MuhammadSaadSheikh@users.noreply.github.com>
2020-01-03 21:15:02 +0500

    Update chat.js

--- js/chat.js
+++ js/chat.js
@@ -1,12 +1,6 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyCagaFwWm9K2dbGEpktlJPQhgKeYMSxNVA",
-  authDomain: "example-bddd0.firebaseapp.com",
-  databaseURL: "https://example-bddd0.firebaseio.com",
-  projectId: "example-bddd0",
-  storageBucket: "example-bddd0.appspot.com",
-  messagingSenderId: "684422025945",
-  appId: "1:684422025945:web:06ce6f5e8ae9031d"
+
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
